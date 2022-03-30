6cf0b6fa2b877f34ba96d4d2c2f4322ac68a6124
hamed shirbandi <hamed.shirbandi@gmail.com>
2022-03-30 19:11:32 +0430

    add some comments for the options

--- MvcPagedList.Core/PagerOptions.cs
+++ MvcPagedList.Core/PagerOptions.cs
@@ -30,23 +30,54 @@ namespace MvcPagedList.Core
         public int TotalItemCount { get; set; }
         public int PageCount { get; set; }
         public int currentPage { get; set; }
-        public bool DisplayInfoArea { get; set; }
-        public bool DisplayPageCountAndCurrentLocation { get; set; }
-        public string CurrentLocationFormat { get; set; }
-        public string PageCountFormat { get; set; }
-        public bool DisplayTotalItemCount { get; set; }
-        public string TotalItemCountFormat { get; set; }
-        public string LinkToNextPageFormat { get; set; }
-        public string LinkToPreviousPageFormat { get; set; }
-        public string WrapperClasses { get; set; }
-        public string UlElementClasses { get; set; }
-        public string LiElementClasses { get; set; }
-
         /// <summary>
-        /// if you dont want to load MvcPagedList.Core.css from cdn you must set this prop to false and link it directly in to your project layout 
+        /// If you dont want to show any information (PageCount,currentPage,TotalItemCount,...) bellow the pagination, set DisplayInfoArea to false
+        /// </summary>
+        public bool DisplayInfoArea { get; set; }
+        /// <summary>
+        /// If you dont want to show PageCount and CurrentLocationFormat bellow the pagination, set DisplayPageCountAndCurrentLocation to false
+        /// </summary>
+        public bool DisplayPageCountAndCurrentLocation { get; set; }
+        /// <summary>
+        /// A lable for currentPage. for example 'page' 1 of 12
+        /// </summary>
+        public string CurrentLocationFormat { get; set; }
+        /// <summary>
+        /// A lable for PageCount. for example page 1 'of' 12
+        /// </summary>
+        public string PageCountFormat { get; set; }
+        /// <summary>
+        /// If you dont want to show TotalItemCount bellow the pagination, set DisplayTotalItemCount to false
+        /// </summary>
+        public bool DisplayTotalItemCount { get; set; }
+        /// <summary>
+        ///  A lable for TotalItemCount. for example 'total item count' 245
+        /// </summary>
+        public string TotalItemCountFormat { get; set; }
+        /// <summary>
+        /// A lable for next btn
+        /// </summary>
+        public string LinkToNextPageFormat { get; set; }
+        /// <summary>
+        /// A lable for previous btn
+        /// </summary>
+        public string LinkToPreviousPageFormat { get; set; }
+        /// <summary>
+        /// If you want to perform your own styles you can use it to add a class for main wrapper of the pagination
+        /// </summary>
+        public string WrapperClasses { get; set; }
+        /// <summary>
+        /// If you want to perform your own styles you can use it to add a class for ul tage
+        /// </summary>
+        public string UlElementClasses { get; set; }
+        /// <summary>
+        /// If you want to perform your own styles you can use it to add a class for li tage
+        /// </summary>
+        public string LiElementClasses { get; set; }
+        /// <summary>
+        /// If you dont want to load MvcPagedList.Core.css from cdn, You must set this to false and add MvcPagedList.Core.css directly in to your project layout 
         /// </summary>
         public bool GetStyleSheetFileFromCdn { get; set; }
-
         /// <summary>
         /// if you dont want to show page numbers just set DisplayPageNumbers to false
         /// </summary>
